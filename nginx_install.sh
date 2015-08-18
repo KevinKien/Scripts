fac794c5a7136afd2c94767403d21b2f2c5a4442
Kevin Kien <kevinkien@hotmail.com>
2015-08-19 00:43:37 +0700

    Scripts install nginx for centos and ubuntu
    Scripts install nginx for centos and ubuntu

--- nginx_install.sh
+++ nginx_install.sh
@@ -2,7 +2,7 @@
 
 # Function Install Nginx On Centos
 
-Nginx_centos() {
+Nginx_Centos() {
     NGINX_VERSION="1.9.0";
     NGINX_MODULE="--user=nginx --group=nginx --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --conf-path=/etc/nginx/nginx.conf --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --with-http_gzip_static_module --with-http_stub_status_module --with-http_ssl_module --with-pcre --with-file-aio --with-http_realip_module --without-http_scgi_module --without-http_uwsgi_module --without-http_fastcgi_module"
 
@@ -95,5 +95,5 @@ if [[ $OS == "Ubuntu" ]]
 then
     Nginx_Ubuntu
 else
-    Nginx_centos
+    Nginx_Centos
 fi
