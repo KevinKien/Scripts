f0dffb595ae43ea8d26d51d97d0499fed392ab20
Kevin Kien <kevinkien@hotmail.com>
2015-08-19 00:30:29 +0700

    Update nginx_install.sh

--- nginx_install.sh
+++ nginx_install.sh
@@ -6,9 +6,19 @@ Nginx_centos() {
     NGINX_VERSION="1.9.0";
     NGINX_MODULE="--user=nginx --group=nginx --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --conf-path=/etc/nginx/nginx.conf --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --with-http_gzip_static_module --with-http_stub_status_module --with-http_ssl_module --with-pcre --with-file-aio --with-http_realip_module --without-http_scgi_module --without-http_uwsgi_module --without-http_fastcgi_module"
 
+    REPO="
+    [nginx]
+    name=nginx repo
+    baseurl=http://nginx.org/packages/centos/$releasever/$basearch/
+    gpgcheck=0
+    enabled=1
+    "
+    
     echo "Install nginx by KevinKien"
 
     yum install gcc gcc-c++ make zlib-devel pcre-devel openssl-devel wget git
+
+    echo $REPO > /etc/yum.repos.d/nginx.repo
 
     wget http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
     tar -xvzf nginx-${NGINX_VERSION}.tar.gz
@@ -46,30 +56,35 @@ Nginx_Ubuntu() {
   
   sudo apt-get install build-essential libc6 libpcre3 libpcre3-dev libpcrecpp0 libssl0.9.8 libssl-dev zlib1g zlib1g-dev lsb-base openssl libssl-dev  libgeoip1 libgeoip-dev  google-perftools libgoogle-perftools-dev libperl-dev  libgd2-xpm-dev libatomic-ops-dev libxml2-dev libxslt1-dev python-dev
   
-  wget http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
-  tar -xvzf nginx-${NGINX_VERSION}.tar.gz
-  cd nginx-${NGINX_VERSION}/
-  ./configure $NGINX_MODULE
-  make
-  make install
+    sudo -s
+    nginx=stable # use nginx=development for latest development version
+    add-apt-repository ppa:nginx/$nginx
+    apt-get update 
   
-  sudo useradd -r nginx
-  wget -O /etc/init.d/nginx http://ctf.ksec.info/nginx-ubuntu
-  sudo chmod +x /etc/init.d/nginx
+    wget http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
+    tar -xvzf nginx-${NGINX_VERSION}.tar.gz
+    cd nginx-${NGINX_VERSION}/
+    ./configure $NGINX_MODULE
+    make
+    make install
+  
+    sudo useradd -r nginx
+    wget -O /etc/init.d/nginx http://ctf.ksec.info/nginx-ubuntu
+    sudo chmod +x /etc/init.d/nginx
 
-  chkconfig --add nginx
-  chkconfig --level 345 nginx on
+    chkconfig --add nginx
+    chkconfig --level 345 nginx on
 
-  echo "
+    echo "
 ----------------------------------------------------------------------------
 NGINX COMPLETED INSTALL
 Checking the nginx version, should be $NGINX_VERSION and you should see our modules
 ----------------------------------------------------------------------------
 "
-  nginx -V
+    nginx -V
 
-  echo "START NGINX!!"
-  sudo /etc/init.d/nginx start
+    echo "START NGINX!!"
+    sudo /etc/init.d/nginx start
 }
 
 # MAIN
